25f9bbe6b6490e3a51fda953780eb8e9c546cbe1
Sergio Peralta <seperalta88@gmail.com>
2023-06-16 10:21:53 -0300

    finish steps 6,7 and 8

--- src/store/users/usersSlice.jsx
+++ src/store/users/usersSlice.jsx
@@ -1,4 +1,17 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+
+export const fetchUsers = createAsyncThunk('users/fetchUsers', async () => {
+	const response = await fetch('https://jsonplaceholder.typicode.com/users');
+	try {
+		if (response.ok) {
+			const users = await response.json();
+			return users;
+		}
+		throw new Error('Request failed!');
+	} catch (error) {
+		console.log(error);
+	}
+});
 
 const initialState = {
 	users: [],
@@ -10,7 +23,19 @@ const usersSlice = createSlice({
 	name: 'users',
 	initialState,
 	reducers: {},
-	extraReducers: {},
+	extraReducers: {
+		[fetchUsers.pending]: (state, action) => {
+			state.isLoading = true;
+		},
+		[fetchUsers.fulfilled]: (state, action) => {
+			state.isLoading = false;
+			state.users = action.payload;
+		},
+		[fetchUsers.rejected]: (state, action) => {
+			state.isLoading = false;
+			state.error = action.error.message;
+		},
+	},
 });
 
 export const {} = usersSlice.actions;
